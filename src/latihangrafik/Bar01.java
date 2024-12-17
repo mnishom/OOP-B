07133d3cbd8a22460c050aeedc6acab280592f34
LABKOM <LABKOM@D1MDOSEN>
2024-12-17 12:17:21 +0700

    update 1

--- src/latihangrafik/Bar01.java
+++ src/latihangrafik/Bar01.java
@@ -5,28 +5,31 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
 import javax.swing.*;
+import org.jfree.chart.plot.PlotOrientation;
 
 public class Bar01 {
     public static void main(String[] args) {
-        // Create a dataset
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        dataset.addValue(1, "januari", "Omset"); 
-        dataset.addValue(2, "februari", "Omset");
-        dataset.addValue(20, "januari", "Item Terjual");               
-        dataset.addValue(16, "februari", "Item Terjual");
-        dataset.addValue(4, "januari", "Jml Transaksi");
-        dataset.addValue(4, "februari", "Jml Transaksi");
+        dataset.addValue(10, "Series1", "Category1");
+        dataset.addValue(20, "Series1", "Category2");
+        dataset.addValue(30, "Series1", "Category3");
+        dataset.addValue(15, "Series2", "Category1");
+        dataset.addValue(25, "Series2", "Category2");
+        dataset.addValue(35, "Series2", "Category3");
 
-        
-        
         // Create the bar chart
         JFreeChart chart = ChartFactory.createBarChart(
-            "Grafik Penjualan",  // Chart title
-            "Category",           // X-axis Label
-            "Value",              // Y-axis Label
-            dataset               // Dataset
+            "Multi-Series Bar Chart",   // Chart title
+            "Category",                 // X-axis Label
+            "Value",                    // Y-axis Label
+            dataset,
+            PlotOrientation.VERTICAL,           // Orientation (vertical)
+            true,                               // Include legend
+            true,                                // Tooltips
+            false                                // URLs// Dataset
         );
-
+        
+       
         // Create and display the chart
         ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
